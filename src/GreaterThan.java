303e7412401f7c2d8c880fa8d7aadf1c960ee5ff
Ana Beatriz <oliveiraanabeatrizde510@gmail.com>
2022-08-28 23:16:50 -0300

    .

--- src/GreaterThan.java
+++ src/GreaterThan.java
@@ -2,19 +2,19 @@ public class GreaterThan {
 
     public static boolean isGreaterThan (int x, int y)  {
         if(isNegativo.isNegativo(x) || isNegativo.isNegativo(y))
-            return x == 0 && y == 0 ? false : x == 0 || y == 0 ? true : isGreaterThan(x+1, y+1);
+            return x == 0 ? true :  y == 0 ? false : isGreaterThan(x+1, y+1);
 
         return x == 0 ? false :  y == 0 ? true : isGreaterThan(x-1,y-1);
 
     }
 
     public static void main(String[] args) {
-        System.out.println(isGreaterThan(1,2)); //TODO: Quando o primeiro é menor da erro
-        System.out.println(isGreaterThan(2,2));
+        System.out.println(isGreaterThan(-1,-2));
+        System.out.println(isGreaterThan(-2,-1));
         System.out.println(isGreaterThan(2,-2));
         System.out.println(isGreaterThan(-2,2));
-        System.out.println(isGreaterThan(-2,-2));
-        System.out.println(isGreaterThan(-4,-2));
+        System.out.println(isGreaterThan(1,2));
+        System.out.println(isGreaterThan(4,4));
 
     }
 }
--- src/LessThan.java
+++ src/LessThan.java
@@ -5,27 +5,21 @@ public class LessThan {
             return x == 0  ? true :  y == 0 ? false : isLessThan(x-1, y-1);
 
         if(isNegativo.isNegativo(x) && isNegativo.isNegativo(y))
+            return x == 0? true : y == 0 ? false : isLessThan(x-1, y+1);
 
-            if ()
+        if(!isNegativo.isNegativo(x) && isNegativo.isNegativo(y))
+            return x == 0? true : y == 0 ? false : isLessThan(x+1, y+1);
 
-            return x == 0  ? true : !isNegativo.isNegativo(x) || !isNegativo.isNegativo(y) ? : y == 0 ? false : isLessThan(x+1, y+1);
-
-        return false;
-
-//        if((isNegativo.isNegativo(x) && isNegativo.isNegativo(y)) && GreaterThan.isGreaterThan(x,y))
-//            return x == 0  ? true :  y == 0 ? false : isLessThan(x+1, y-1);
-//
-//        return x == 0 ? false :  y == 0 ? true : isLessThan(x+1,y-1);
-
+        return x == 0 ? true : y == 0 ? false : isLessThan(x+1, y-1);
     }
 
     public static void main(String[] args) {
-        System.out.println(isLessThan(-1,-2));
-        System.out.println(isLessThan(-2,-1));
-//        System.out.println(isLessThan(2,-2));
-//        System.out.println(isLessThan(-2,2)); //
-//        System.out.println(isLessThan(-2,-2));
-//        System.out.println(isLessThan(-4,-2));
+        System.out.println(isLessThan(2,1));
+        System.out.println(isLessThan(1,2));
+        System.out.println(isLessThan(-2,-2));
+        System.out.println(isLessThan(-2,2));
+        System.out.println(isLessThan(2,-8));
+        System.out.println(isLessThan(-1,2));
 
     }
 }
